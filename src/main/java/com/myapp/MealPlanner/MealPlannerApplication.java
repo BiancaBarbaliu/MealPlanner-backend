296ae2dbf508a20041d5bee028a8a9237d98e676
BiancaBarbaliu <bianca.barbaliu02@e-uvt.ro>
2024-01-20 20:00:54 +0200

    Initial commit

---
+++ src/main/java/com/myapp/MealPlanner/MealPlannerApplication.java
@@ -0,0 +1,14 @@
+package com.myapp.MealPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlannerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!!!");
+		SpringApplication.run(MealPlannerApplication.class, args);
+	}
+
+}
