ee7ea4d0d54fbb217d7529c22c239f37363b2dc2
BiancaBarbaliu <bianca.barbaliu02@e-uvt.ro>
2024-01-21 19:07:10 +0200

    Implement authentication backend

---
+++ src/main/java/com/myapp/MealPlanner/DataSourceConfig.java
@@ -0,0 +1,42 @@
+package com.myapp.MealPlanner;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUsername("admin");
+        dataSource.setPassword("password");
+        dataSource.setSchema("MealPlanner");
+        dataSource.setUrl(
+                "jdbc:postgresql://localhost:5432/postgres");
+
+
+        return dataSource;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
